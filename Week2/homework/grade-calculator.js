9cbee80a1cb892a0de5ceb9754a6533588326954
Ayda <aydahagos@yahoo.com>
2020-09-19 18:23:01 +0200

    Grade calculator edited

--- Week2/homework/grade-calculator.js
+++ Week2/homework/grade-calculator.js
@@ -1,30 +1,25 @@
-
-//A function that prints out grade and the value of val if the condition of the if / of else if fulfills
-function grade(val){
-    if((val>=0)&&(val <= 49)){   
-        console.log(`You got F (${val}%)!`);  
-    }
-    else if ((val >= 50) && (val <= 59)){
-        console.log(`You got E (${val}%)!`);
-    }
-    else if ((val >= 60) && (val <= 69)){
-        console.log(`You got a D (${val}%)!`);
-    }
-    else if ((val >= 70) && (val <= 79)){
-        console.log(`You got a C (${val}%)!`);
-    }
-    else if ((val >= 80) && (val <= 89)){
-        console.log(`You got a B ({val}%)!`);
-    }
-    else if ((val >= 90) && (val <= 100)){
-        console.log(`You got A (${val}%)!`);
-    }
-    else{
-        console.log("input is not valid");
-    }
+//A function that prints out grade and the resultue of result if the condition of the if / of else if fulfills
+function grade(result) {
+  let grade;
+  if (result >= 0 && result <= 49) {
+    grade = "F";
+  } else if (result >= 50 && result <= 59) {
+    grade = "E";
+  } else if (result >= 60 && result <= 69) {
+    grade = "D";
+  } else if (result >= 70 && result <= 79) {
+    grade = "C";
+  } else if (result >= 80 && result <= 89) {
+    grade = "B";
+  } else if (result >= 90 && result <= 100) {
+    grade = "A";
+  } else {
+    console.log("result must be a number between 0 and 100"); //prints out what the value of result should be.
+  }
+  console.log(`You got ${grade} (${result}%)!`); //prints out the final grade 
 }
 //calling the function grade()
-grade(98); 
+grade(98);
 grade(42);
 grade(62);
 grade(72);
